3e308bcdc6590904aa96cad82f923aa0308cf71c
LRYP <2213469156@qq.com>
2025-07-07 22:18:01 +0800

    增加了返回阶段1的API

--- SemesterPhaseService/APIs/RollBackToPhase1Message.ts
+++ SemesterPhaseService/APIs/RollBackToPhase1Message.ts
@@ -1,14 +1,13 @@
 /**
- * RollBackToPhase1Service
+ * RollBackToPhase1Message
  * desc: 清空所有选课信息，保留用户与开课信息
  * @param adminToken: String (管理员token)
- * @return true
  */
 import { TongWenMessage } from 'Plugins/TongWenAPI/TongWenMessage'
 
 
 
-export class RollBackToPhase1Service extends TongWenMessage {
+export class RollBackToPhase1Message extends TongWenMessage {
     constructor(
         public  adminToken: string
     ) {
